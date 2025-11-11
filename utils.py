079d5b13592eb72440dbde5d41fdd221e51d1b56
Eduardo Osuna <osunabusiness64@gmail.com>
2025-11-11 00:18:22 -0500

    Updated utils.py
    This updates the file to always use American/New_York time zone when recording files.

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from typing import Optional
 import sqlite3
+from zoneinfo import ZoneInfo
 
 def log_access(conn: sqlite3.Connection, user_id: Optional[int], action: str, success: int):
     c = conn.cursor()
-    timestamp = datetime.now().isoformat()
+    timestamp = datetime.now(ZoneInfo("America/New_York")).isoformat()
     c.execute("INSERT INTO access_logs(user_id, action, timestamp, success) VALUES (?, ?, ?, ?)", (user_id, action, timestamp, success))
     conn.commit()
